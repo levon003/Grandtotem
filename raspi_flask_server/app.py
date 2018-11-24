3bb0e3a2be6f142eb9bd750c66591fae8ad72744
Yangyangl7 <cmlyy0618@gmail.com>
2018-11-24 00:45:45 -0600

    Complete gallery functions

--- raspi_flask_server/app.py
+++ raspi_flask_server/app.py
@@ -2,22 +2,58 @@ from flask import Flask, render_template, redirect, url_for, request
 import RPi.GPIO as io
 import threading
 import requests
+import os
 
 io.setmode(io.BCM)
 door_pin = 23
 io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
 
+dir = './static/gdrive'
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return redirect(url_for('gallery'))
 
-@app.route('/photo')
-def photo():
-    return render_template('photo.html', 
-                      photoUrl=request.args.get('photoUrl'))
+@app.route('/gallery')
+def gallery():
+    imgList = loadImg()
+    videoList = loadVideo()
+    return render_template('home.html', imgList=imgList, videoList=videoList)
 
+@app.route('/gallery/update', methods=['POST'])
+def galleryUpdate():
+    if request.method == 'POST':
+        imgList = loadImg()
+        videoList = loadVideo()
+        return render_template('home.html', imgList=imgList, videoList=videoList)
+
+@app.route('/gallery/<string:fileName>')
+def selection(fileName):
+    json = {'fileName':fileName}
+    res = requests.post('http://127.0.0.1:5001/gallery/selection', json=json)
+
+def loadImg():
+    imgList = []
+    for filename in os.listdir(dir):
+        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            imgList.append(filename)
+        else:
+            continue
+    
+    return imgList
+
+def loadVideo():
+    videoList = []
+    for filename in os.listdir(dir):
+        if filename.endswith(".webm") or filename.endswith(".mp4"):
+            videoList.append(filename)
+        else:
+            continue
+        
+    return videoList
+            
 def check_pin(prev_state):
     switch_state = io.input(door_pin)
     if switch_state:
@@ -26,7 +62,6 @@ def check_pin(prev_state):
     else:
 	test_json = {'request_type': 'deactivate'}
         print("close")
-    
     
     threading.Timer(10.0, check_pin, [switch_state]).start()
     
