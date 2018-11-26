5acc8ae1b453bae886e407d288829cbfa4e0f91a
Irene Ye Yuan <yuan0191@umn.edu>
2018-11-25 19:31:14 -0600

    Update mapping touch postion in camera view, remove key press in slideshow view, add video format for mp4 in template

--- pc_flask_server/static/js/slideshow.js
+++ pc_flask_server/static/js/slideshow.js
@@ -2,36 +2,6 @@ $( document ).ready(function() {
     window.setInterval(intervalFunction, 100);
 });
 
-$(document).keypress(function(e) {
-  if(e.which == 13) {
-    console.log("keypressed");
-    window.location.href = "/video";
-    //console.log("{{ url_for('video') }}");
-  }
-  if(e.which == 32) {
-    var file_name = {file_name: '6.jpg'}
-    $.ajax({
-        url: '/slideshow',
-        type: "POST",
-        dataType: 'json',
-        data: JSON.stringify(file_name),
-        processData: false,
-        contentType: false,
-        success: function(response) {
-            console.log("reload");
-        },
-        error: function(jqXHR, textStatus, errorMessage) {
-            //alert('Error:' + JSON.stringify(errorMessage));
-        },
-        complete: function(data) {
-            redirect = "http://127.0.0.1:5001/view?selected=6.jpg"
-            window.location.href = redirect;
-        }
-    });
-
-  }
-});
-
 function intervalFunction() {
     shouldFileBeDisplayed();
     shouldCameraViewBeActive();
@@ -47,6 +17,7 @@ function shouldFileBeDisplayed() {
             } else {
                 let filename = response;
                 let redirect = "http://127.0.0.1:5001/view?selected=" + filename;
+                console.log(redirect);
                 window.location.href = redirect;
             }
         },
